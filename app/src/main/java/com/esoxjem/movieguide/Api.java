ebbeccc1bdf6f8c54386b9c3cadbce2accca97f9
Ashwini Kumar <ashwini.kumar@1mg.com>
2016-10-15 12:46:48 +0530

    Removed key

--- app/src/main/java/com/esoxjem/movieguide/Api.java
+++ app/src/main/java/com/esoxjem/movieguide/Api.java
@@ -5,7 +5,7 @@ package com.esoxjem.movieguide;
  */
 public class Api
 {
-    public static final String API_KEY = "eca1e53518090eb02f1180ab48607705"; // add your API key here
+    public static final String API_KEY = ""; // add your API key here
     public static final String POSTER_PATH = "http://image.tmdb.org/t/p/w342";
     public static final String BACKDROP_PATH = "http://image.tmdb.org/t/p/w780";
     public static final String YOUTUBE_VIDEO_URL = "http://www.youtube.com/watch?v=%1$s";
